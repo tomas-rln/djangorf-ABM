6bed50588c2b11146a312778c63c3c575cf5ae0d
tomas-rln <tomas.ezequiel.rln@hotmail.com>
2021-04-24 22:53:04 -0300

    Deleted some dependencies

--- src/api/api.py
+++ src/api/api.py
@@ -1,6 +1,5 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view
 
 from api.serializers import UserSerializer
 from apps.user.models import User

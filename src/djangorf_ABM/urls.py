67bf5696a5a256c9fadc6bb2ff0d6af4dbcb2298
tomas-rln <tomas.ezequiel.rln@hotmail.com>
2021-04-24 21:59:22 -0300

    Principal urls corrected

--- src/djangorf_ABM/urls.py
+++ src/djangorf_ABM/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users', include('apps.user.urls')),
+    path('users/', include('apps.user.urls')),
 ]
